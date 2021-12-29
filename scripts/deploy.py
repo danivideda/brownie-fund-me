27ab6d79b41c177f2cc08c6e36eb1affdca405e0
Dani Ihza Farrosi <dani.videda@gmail.com>
2021-12-29 22:04:28 +0700

    verifiy source on etherscan

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,8 +4,8 @@ from scripts.helper import get_account
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from": account})
-    print(f"Contract address: {fund_me}")
+    fund_me = FundMe.deploy({"from": account}, publish_source=True)
+    print(f"Contract deployed: {fund_me}")
 
 def main():
     deploy_fund_me()
